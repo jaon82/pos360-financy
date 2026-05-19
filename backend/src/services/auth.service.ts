7a0e821e6c425c22eaa768a7f9bc42b7738b0e27
Jean Nunes <jaon82@gmail.com>
2026-05-18 23:37:21 -0300

    feat: Implement login logic in Auth Service

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,11 +1,19 @@
 import { prismaClient } from '../../prisma/prisma';
-import type { RegisterInput } from '../dtos/input/auth.input';
-import type { RegisterOutput } from '../dtos/output/auth.output';
+import type { LoginInput, RegisterInput } from '../dtos/input/auth.input';
+import type { LoginOutput, RegisterOutput } from '../dtos/output/auth.output';
 import type { UserModel } from '../models/user.model';
-import { hashPassword } from '../utils/hash';
+import { comparePassword, hashPassword } from '../utils/hash';
 import { signJwt } from '../utils/jwt';
 
 export class AuthService {
+  generateToken(user: UserModel): RegisterOutput {
+    const token = signJwt({ id: user.id, email: user.email }, '15m');
+
+    const refreshToken = signJwt({ id: user.id, email: user.email }, '1d');
+
+    return { token, refreshToken, user };
+  }
+
   async register(data: RegisterInput): Promise<RegisterOutput> {
     const existingUser = await prismaClient.user.findUnique({
       where: { email: data.email },
@@ -28,11 +36,24 @@ export class AuthService {
     return this.generateToken(user);
   }
 
-  generateToken(user: UserModel): RegisterOutput {
-    const token = signJwt({ id: user.id, email: user.email }, '15m');
+  async login(data: LoginInput): Promise<LoginOutput> {
+    const existingUser = await prismaClient.user.findUnique({
+      where: { email: data.email },
+    });
 
-    const refreshToken = signJwt({ id: user.id, email: user.email }, '1d');
+    if (!existingUser) {
+      throw new Error('Email ou senha inválidos');
+    }
 
-    return { token, refreshToken, user };
+    const isPasswordValid = await comparePassword(
+      data.password,
+      existingUser.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new Error('Email ou senha inválidos');
+    }
+
+    return this.generateToken(existingUser);
   }
 }
