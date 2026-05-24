77c6ca2aa2291b8ac3ca0cffdb4ee64fea6bf301
Jean Nunes <jaon82@gmail.com>
2026-05-24 04:01:35 -0300

    feat(app): integrate and demonstrate new Button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { Mail } from 'lucide-react';
+import { Button } from './components/ui/button';
 import { Field, FieldDescription, FieldLabel } from './components/ui/field';
 import { Input } from './components/ui/input';
 
 function App() {
   return (
-    <div className="border h-50 display flex items-center justify-center max-w-5xl m-0 m-auto">
+    <div className="border h-screen display flex flex-col gap-2 items-center justify-center max-w-5xl my-0 mx-auto">
       <Field>
         <FieldLabel htmlFor="name">Label</FieldLabel>
         <Input
@@ -18,6 +19,21 @@ function App() {
         />
         <FieldDescription>Helper/Error</FieldDescription>
       </Field>
+      <Button size="sm">
+        <Mail />
+        Button
+      </Button>
+      <Button variant="outline" size="sm" disabled>
+        <Mail />
+        Button
+      </Button>
+      <Button size="icon" variant="outline" disabled>
+        <Mail color="red" />
+      </Button>
+      <Button variant="link">Link</Button>
+      <Button variant="outline" size="icon" disabled>
+        1
+      </Button>
     </div>
   );
 }
