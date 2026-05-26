fcba2bc508cd53c9f60cb976319d88882b6a1f63
Jean Nunes <jaon82@gmail.com>
2026-05-26 17:58:53 -0300

    Feat: Add reusable PageHeader component

---
+++ frontend/src/components/PageHeader/index.tsx
@@ -0,0 +1,29 @@
+import { Plus } from 'lucide-react';
+import { Button } from '../ui/button';
+
+interface PageHeaderProps {
+  title: string;
+  description: string;
+  buttonText: string;
+}
+
+export default function PageHeader({
+  title,
+  description,
+  buttonText,
+}: PageHeaderProps) {
+  return (
+    <div className="flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl font-bold text-gray-800">{title}</h1>
+        <p className="text-gray-600">{description}</p>
+      </div>
+      <div>
+        <Button>
+          <Plus className="w-4" />
+          {buttonText}
+        </Button>
+      </div>
+    </div>
+  );
+}
