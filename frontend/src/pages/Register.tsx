f68e5f908edab6c7bf676c2db39f6f2b97100f62
Jean Nunes <jaon82@gmail.com>
2026-05-26 13:19:44 -0300

    feat(frontend): Integrate authentication with Register page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Lock, LogIn, Mail, UserRound } from 'lucide-react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router';
+import { toast } from 'sonner';
 import z from 'zod';
 import { Button } from '@/components/ui/button';
 import {
@@ -14,9 +16,12 @@ import {
 import { Field, FieldDescription, FieldLabel } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
+import { useAuthStore } from '@/store/auth';
 import Logo from '../assets/logo.svg';
 
 export default function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const signUp = useAuthStore((state) => state.register);
   const registerFormSchema = z.object({
     name: z.string(),
     email: z.email('Digite um e-mail válido'),
@@ -32,7 +37,22 @@ export default function Register() {
     resolver: zodResolver(registerFormSchema),
   });
 
-  const onSubmit = (data: RegisterForm) => console.log(data);
+  const onSubmit = async (formData: RegisterForm) => {
+    setIsLoading(true);
+    try {
+      const success = await signUp(formData);
+      if (success) {
+        toast.success('Cadastro realizado com sucesso!');
+      } else {
+        toast.error('Falha ao realizar o cadastro. Tente novamente.');
+      }
+    } catch (error) {
+      console.error('Erro ao fazer cadastro:', error);
+      toast.error('Ocorreu um erro ao realizar o cadastro.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <div className="flex flex-col gap-8 p-12 items-center justify-center">
@@ -82,7 +102,9 @@ export default function Register() {
                 A senha deve ter no mínimo 8 caracteres
               </FieldDescription>
             </Field>
-            <Button type="submit">Cadastrar</Button>
+            <Button type="submit" disabled={isLoading}>
+              {isLoading ? 'Cadastrando...' : 'Cadastrar'}
+            </Button>
           </form>
           <div className="grid grid-cols-[1fr_50px_1fr] items-center mt-6">
             <Separator />
